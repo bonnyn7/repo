f5921410f2d9d41274f91d16f480e3f9c9cb424b
Anastasia <bonnyn7@yandex.ru>
2022-06-24 14:57:15 +0300

    Added prog6

---
+++ Example006_ConditionIfElse/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите имя пользователя");
+        string username = Console.ReadLine();
+
+        if (username.ToLower() == "маша")
+        {
+            Console.WriteLine("Ура, это же Маша !");
+        }
+        else
+        {
+            Console.Write("Привет, ");
+            Console.WriteLine(username);
+        }
+    }
+}
